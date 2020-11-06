43db067d7161f334de25cae0fffe8e0722ffb2aa
rebec <ralegria77@gmail.com>
2020-11-05 21:53:00 -0800

    commit tree 59b660ae41f4050ca66f435cc0e70dc73db5c2a5

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+# dog needs a name and a breed
+
+class Dog
+    attr_accessor :name, :breed
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.breed = "Beagle"
+
+
+    
+
+
+
+
+
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+    attr_accessor :name, :job
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+beyonce.job = "Singer"
